2ffce25479be660791051aa27a8f0da541ae7ea4
Adilson Lima <adilsonlima85@hotmail.com>
2020-07-31 15:55:06 -0300

    Setup do projeto

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo() {
+  return (
+    <div>
+      Página de Cadastro de vídeo
+    </div>
+  )
+}
+ 
+const Pagina404 = () => (<div>Página 404</div>)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+    <Route path="/" component={App} exact />
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route component={Pagina404} />
+    
+    </Switch>
+
+  </BrowserRouter>,
+  
+  document.getElementById('root')
+);
+
